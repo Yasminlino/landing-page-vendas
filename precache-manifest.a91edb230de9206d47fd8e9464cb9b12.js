ee49c7dcb85897d73f020aef48e8efc2d176a87e
Yasmin <yasmin.lino@sisand.com.br>
2025-05-13 11:17:24 -0300

    Updates

---
+++ precache-manifest.a91edb230de9206d47fd8e9464cb9b12.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2effd5710e160df8f8d0b845a4892e8f",
+    "url": "/landing-page-vendas/index.html"
+  },
+  {
+    "revision": "8c11514328505f126317",
+    "url": "/landing-page-vendas/static/css/2.7446b4c7.chunk.css"
+  },
+  {
+    "revision": "c023dec256c96172b9e5",
+    "url": "/landing-page-vendas/static/css/main.334fe39b.chunk.css"
+  },
+  {
+    "revision": "8c11514328505f126317",
+    "url": "/landing-page-vendas/static/js/2.d70bbdea.chunk.js"
+  },
+  {
+    "revision": "0ae889b0d2cac9dfef788b04a9f4e825",
+    "url": "/landing-page-vendas/static/js/2.d70bbdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed0befad8002e12b4b0",
+    "url": "/landing-page-vendas/static/js/3.93b7bac0.chunk.js"
+  },
+  {
+    "revision": "c023dec256c96172b9e5",
+    "url": "/landing-page-vendas/static/js/main.332357e5.chunk.js"
+  },
+  {
+    "revision": "e60def0de89e36ed9644",
+    "url": "/landing-page-vendas/static/js/runtime-main.b3b856b6.js"
+  },
+  {
+    "revision": "d75f4f6b5907ae4470006f85352a6b0b",
+    "url": "/landing-page-vendas/static/media/Background1.d75f4f6b.webp"
+  },
+  {
+    "revision": "2099c20a042e3dde79c480e78877b2b4",
+    "url": "/landing-page-vendas/static/media/Background2.2099c20a.webp"
+  },
+  {
+    "revision": "4d78e1ed220f2a7332efaeef22ed75f5",
+    "url": "/landing-page-vendas/static/media/Fabricio-de-Souza.4d78e1ed.webp"
+  },
+  {
+    "revision": "c2e4f0f03f3bd4c017d53c848a1740cd",
+    "url": "/landing-page-vendas/static/media/Imagem-capa-desktop.c2e4f0f0.webp"
+  },
+  {
+    "revision": "3da5df58124d53ce039885056aad70ea",
+    "url": "/landing-page-vendas/static/media/Imagem-capa-mobile.3da5df58.webp"
+  },
+  {
+    "revision": "9563f7f972b940ab436dc9ecf8a3c2b7",
+    "url": "/landing-page-vendas/static/media/Imagem-capa-mobile2.9563f7f9.webp"
+  },
+  {
+    "revision": "0a7ec1ae55a7224a4e0f03386d75e731",
+    "url": "/landing-page-vendas/static/media/a.0a7ec1ae.jpg"
+  },
+  {
+    "revision": "60616752cb1ca297bbe34ca87a7668ce",
+    "url": "/landing-page-vendas/static/media/a.60616752.webp"
+  },
+  {
+    "revision": "37165a6d307ba8653819c95d57af074f",
+    "url": "/landing-page-vendas/static/media/b.37165a6d.jpg"
+  },
+  {
+    "revision": "86b71e29ce9bab03e41d2f7efaa4db48",
+    "url": "/landing-page-vendas/static/media/b.86b71e29.webp"
+  },
+  {
+    "revision": "7a780d90290a4ec3b40e41766b0700fc",
+    "url": "/landing-page-vendas/static/media/c.7a780d90.webp"
+  },
+  {
+    "revision": "a889acae6636542a9de93e930638093a",
+    "url": "/landing-page-vendas/static/media/c.a889acae.jpg"
+  },
+  {
+    "revision": "58db368368ebfb5351c872013dfe6ee5",
+    "url": "/landing-page-vendas/static/media/d.58db3683.webp"
+  },
+  {
+    "revision": "a6bc6c7363fd8a79ae0118ed0cf9171c",
+    "url": "/landing-page-vendas/static/media/e.a6bc6c73.webp"
+  },
+  {
+    "revision": "58dd03443c27f095819a184316bf026c",
+    "url": "/landing-page-vendas/static/media/f.58dd0344.webp"
+  },
+  {
+    "revision": "3cf359a4aba38c5cffa75287c9bf9079",
+    "url": "/landing-page-vendas/static/media/g.3cf359a4.webp"
+  },
+  {
+    "revision": "bcb3d005fd0adc663f177142a5fdcfdb",
+    "url": "/landing-page-vendas/static/media/h.bcb3d005.webp"
+  },
+  {
+    "revision": "d27fbf16fc4ca8ec7b0bb0e437496c0d",
+    "url": "/landing-page-vendas/static/media/i.d27fbf16.webp"
+  },
+  {
+    "revision": "946e6c51a52ff576a70ab76f728c9d52",
+    "url": "/landing-page-vendas/static/media/j.946e6c51.webp"
+  },
+  {
+    "revision": "c1ef28e4cd2bba89afe488c29e17cddd",
+    "url": "/landing-page-vendas/static/media/k.c1ef28e4.webp"
+  },
+  {
+    "revision": "5ffe1f0b87d5f295f3113ae526e1a8eb",
+    "url": "/landing-page-vendas/static/media/l.5ffe1f0b.webp"
+  },
+  {
+    "revision": "75ec5ba2452db4ddcf052b504d33d734",
+    "url": "/landing-page-vendas/static/media/logo.75ec5ba2.webp"
+  },
+  {
+    "revision": "b3fcaa1bcc45060dd2fffa93792f4c3c",
+    "url": "/landing-page-vendas/static/media/m.b3fcaa1b.webp"
+  },
+  {
+    "revision": "0581013488d833afcf5c99183f891c42",
+    "url": "/landing-page-vendas/static/media/n.05810134.webp"
+  },
+  {
+    "revision": "e7ebc1f7eabd08a5a0edc50034bd33b0",
+    "url": "/landing-page-vendas/static/media/o.e7ebc1f7.webp"
+  }
+]);
