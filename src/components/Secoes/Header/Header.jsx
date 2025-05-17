9f3eb368260482bbbca289880efac6696bc0c435
Yasmin <yasmin.lino@sisand.com.br>
2025-05-17 02:34:49 -0300

    ajustes de performance

--- src/components/Secoes/Header/Header.jsx
+++ src/components/Secoes/Header/Header.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import logo from '../../../assets/images/logo.webp';
 import "./Header.css";
 
 export default function Header({ timeLeft }) {
-    return (
-        <div>
-            <div className="alerta-topo">
-                <span role="img" aria-label="Relógio">⏰</span> Oferta por tempo limitado: <strong>Garanta seu acesso com desconto!</strong>
-                <span className="contador">
-                    <span role="img" aria-label="Calendário">📅</span> {timeLeft.days} dias, <span role="img" aria-label="Relógio de areia">⌛</span> {timeLeft.hours}:{timeLeft.minutes}:{timeLeft.seconds}
-                </span>
-            </div>
+  return (
+    <div>
+      <Helmet>
+        <link rel="preload" as="image" href="/assets/images/Imagem-capa-desktop.webp" />
+      </Helmet>
+      <div className="alerta-topo">
+        <span role="img" aria-label="Relógio">⏰</span> Oferta por tempo limitado: <strong>Garanta seu acesso com desconto!</strong>
+        <span className="contador">
+          <span role="img" aria-label="Calendário">📅</span> {timeLeft.days} dias, <span role="img" aria-label="Relógio de areia">⌛</span> {timeLeft.hours}:{timeLeft.minutes}:{timeLeft.seconds}
+        </span>
+      </div>
 
-            {/* Seção do Header */}
-            <section className="section1 pedding-top section-header d-flex justify-content-center">
-                <div className="container d-flex align-items-end">
-                    <div className="row">
-                        <div className="logo-header">
-                            <img src={logo} alt="Logo" />
-                        </div>
-                        <div>
-                            <h1 className="display-5 anton-regular corAmarela">
-                                ELIMINE <span className="color-white">ATÉ 10KG</span> EM <span>30 DIAS</span> COMO O MÉTODO QUE <span className="color-white">REALMENTE FUNCIONA.</span>
-                            </h1>
-                            <p className="lead mt-3 color-white texto-header">
-                                Você não precisa sofrer, passar fome ou viver na academia. 
-                                O Método Evolution é <span className='corAmarela'>leve, estratégico e se encaixa no seu dia a dia.</span> Mesmo que você não goste de academia. Mesmo que sua rotina seja corrida. 
-                                Esse método foi feito pra você que quer emagrecer de forma definitiva.
-                            </p>
-                            <a href="#precos" className="botaoPrincipal botaoPrincipalHeader">
-                                Quero o resultado que sempre sonhei!
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      {/* Seção do Header */}
+      <section className="section1 padding-top section-header d-flex justify-content-center">
+        <div className="container d-flex align-items-end">
+          <div className="row">
+            <div className="logo-header">
+              <img src={logo} alt="Logo" />
+            </div>
+            <div>
+              <h1 className="display-5 anton-regular corAmarela">
+                ELIMINE <span className="color-white">ATÉ 10KG</span> EM <span>30 DIAS</span> COMO O MÉTODO QUE <span className="color-white">REALMENTE FUNCIONA.</span>
+              </h1>
+              <p className="lead mt-3 color-white texto-header">
+                Você não precisa sofrer, passar fome ou viver na academia.
+                O Método Evolution é <span className='corAmarela'>leve, estratégico e se encaixa no seu dia a dia.</span> Mesmo que você não goste de academia. Mesmo que sua rotina seja corrida.
+                Esse método foi feito pra você que quer emagrecer de forma definitiva.
+              </p>
+              <a href="#precos" className="botaoPrincipal botaoPrincipalHeader">
+                Quero o resultado que sempre sonhei!
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
